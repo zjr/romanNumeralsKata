d2a76777d7f52cbe53e51f8f5d7cc827f7a83cf0
Zachary J Rollyson <zach@izimobile.com>
2016-08-10 00:02:20 -0400

    Replace method with one somewhat more scalable.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
 'use strict';
 
+const numerals = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+
+const getNumeralsForPlace = (int, placeIdx = 0) => {
+  const numeralIdx = placeIdx * 2;
+  const numeral = numerals[numeralIdx];
+  const halfStep = numerals[numeralIdx + 1];
+  const fullStep = numerals[numeralIdx + 2];
+
+  let str = '';
+
+  if (int === 9) {
+    return numeral + fullStep;
+  } else if (int === 4) {
+    return numeral + halfStep;
+  } else if (int >= 5) {
+    str += halfStep;
+    int -= 5;
+  }
+
+  while (int > 0) {
+    str += numeral;
+    int--;
+  }
+
+  return str;
+};
+
 module.exports = {
   arabicToNumeral(int) {
-    let str = '';
-    if (int === 9) {
-      str = 'IX';
-      int = 0;
-    }
-    if (int / 5 >= 1) {
-      str += 'V';
-      int -= 5;
-    }
-    if (int === 4) {
-      str += 'IV';
-    }
-    if (int <= 3) {
-      for (let i = 0; i < int; i++) {
-        str += 'I';
-      }
-    }
-    return str;
+    return getNumeralsForPlace(int)
   }
 };
